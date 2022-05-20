46925b58353d03f8314fd587293ffc9562a5343d
NgocTrungTran <milotran1496@gmail.com>
2022-05-20 09:32:05 +0700

    BT Interface Comparable

---
+++ AbstractAndInterface/src/BTInterface_Comparable/Shape.java
@@ -0,0 +1,49 @@
+package BTInterface_Comparable;
+
+public class Shape {
+    private String color = "green";
+    private boolean filled = true;
+
+    public Shape() {
+
+    }
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public  String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String toString() {
+        return "A Shape with color of "
+                + getColor ()
+                +" and "
+                + (isFilled () ? "filled" : "not filled");
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+    }
+}
