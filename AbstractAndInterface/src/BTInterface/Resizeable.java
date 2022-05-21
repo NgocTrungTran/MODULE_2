b4caa73c38499e59a2bd9fd5c9058a4fe7408576
NgocTrungTran <milotran1496@gmail.com>
2022-05-21 16:56:53 +0700

    BT Interface Resize

---
+++ AbstractAndInterface/src/BTInterface/Resizeable.java
@@ -0,0 +1,5 @@
+package BTInterface;
+
+public interface Resizeable {
+    public abstract double resize(double percent);
+}
---
+++ AbstractAndInterface/src/BTInterface/TestResize.java
@@ -0,0 +1,21 @@
+package BTInterface;
+
+public class TestResize {
+    public static void main(String[] args) {
+        int x = (int) Math.floor ( Math.random () * 10);
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle (x, "Blue", false);
+        shapes[1] = new Rectangle (x, x, "White", true);
+        shapes[2] = new Square (x, "Violet", false);
+        System.out.println ("Before resize: ");
+        for (Shape a : shapes) {
+            System.out.println (a);
+        }
+
+        System.out.println ("After resize: ");
+        for (Shape a : shapes){
+            a.resize ( Math.random () * 100);
+            System.out.println (a);
+        }
+    }
+}
