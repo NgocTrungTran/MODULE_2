00125b87ab53fd788b229a2bc5a38ad9373d0147
NgocTrungTran <milotran1496@gmail.com>
2022-05-29 13:08:41 +0700

    TH trien khai linkedLis

---
+++ ArrayList_LinkedList/src/THLinkedList/TestMylinkedList.java
@@ -0,0 +1,15 @@
+package THLinkedList;
+
+public class TestMylinkedList {
+    public static void main(String[] args) {
+        System.out.println ("/=/=/=/=/= TESTING /=/=/=/=/=");
+        MylinkedList ll = new MylinkedList ( 10 );
+        ll.addFirst ( 11 );
+        ll.addFirst ( 12 );
+        ll.addFirst ( 13 );
+
+        ll.add ( 4, 9 );
+        ll.add ( 1, 9 );
+        ll.printList();
+    }
+}
