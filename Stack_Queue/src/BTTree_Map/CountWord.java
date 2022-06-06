cdbe4ca7371d6b1ed21e25001a15f02baef4e6ac
NgocTrungTran <milotran1496@gmail.com>
2022-06-06 20:06:19 +0700

    BT Dem Chu bang Tree Map

---
+++ Stack_Queue/src/BTTree_Map/CountWord.java
@@ -0,0 +1,33 @@
+package BTTree_Map;
+
+import java.util.Set;
+import java.util.TreeMap;
+
+public class CountWord {
+    public static void main(String[] args) {
+        String str = "Tran Ngoc Trung. C0322G1, trung, Codegym Hue";
+        str.replace ( ",", "" );
+        str.replace ( ".", "" );
+        String[] array = str.split ( "" );
+        String key = "";
+        int value;
+        TreeMap<String, Integer> map = new TreeMap<> ();
+        for (String s : array) {
+            key = s.toLowerCase ();
+            if ( map.containsKey ( key ) ) {
+                value = map.get ( key );
+                map.remove ( key );
+                map.put ( key, value + 1 );
+            } else {
+                map.put ( key, 1 );
+            }
+        }
+        Object o = new Object ();
+        Set<String> set = map.keySet ();
+        for (String s : set) {
+            key = s;
+            System.out.println ( key + " show "
+                    + map.get ( key ) + " times." );
+        }
+    }
+}
